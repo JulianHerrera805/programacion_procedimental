c69089221c4d76249188505bdcef34242553b99a
JulianHerrera805 <julian.herreras@autonoma.edu.co>
2026-07-18 11:46:27 -0500

    structure functions all modules

--- js/inventario.js
+++ js/inventario.js
@@ -1,55 +1,82 @@
-let categoria = document.getElementById("CategoriaProducto").value;
-let Nombre = document.getElementById("NombreProducto").value;
-let precioUnitario = document.getElementById("PrecioUnitario").value;
-let cantidad = document.getElementById("CantidadProducto").value;
-let codigo = document.getElementById("CodigoProducto").value;
-let marca = document.getElementById("Marcaproducto").value;
-let UnidadDeMedida = document.getElementById("unidadMedida").value;
+function ValidarInformacion() {
+    let categoria = document.getElementById("CategoriaProducto").value;
+    let Nombre = document.getElementById("NombreProducto").value;
+    let precioUnitario = document.getElementById("PrecioUnitario").value;
+    let cantidad = document.getElementById("CantidadProducto").value;
+    let codigo = document.getElementById("codigoProducto").value;
+    let marca = document.getElementById("MarcaProducto").value;
+    let UnidadDeMedida = document.getElementById("unidadMedida").value;
 
-function ValidarDatos() {
-    if (CategoriaProducto != '' || NombreProducto != '') {
-        if (CategoriaProducto == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        if (NombreProducto == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
+    if (!categoria || !Nombre || !precioUnitario || !cantidad || !codigo || !marca || !UnidadDeMedida) {
+        Swal.fire({
+            title: "campos incompletos",
+            text: "rellena los campos",
+            icon: "question"
+        });
     }
     else {
-        console.log("ingrese todos los datos")
-    }
+        console.log(
+            `Informacion del Producto: \n
+            ${categoria} \n
+            ${Nombre} \n
+            ${precioUnitario} \n
+            ${cantidad} \n
+            ${codigo} \n
+            ${marca} \n
+            ${UnidadDeMedida} \n`
+        );
+        if (!/^[a-zA-Z]+$/.test(categoria)) {
+            console.log("La Categoria debe contener letras")
+            Swal.fire("La categoria debe tener letras");
+            return;
+        }
+        if (!/^[a-zA-Z]+$/.test(Nombre)) {
+            console.log("El nombre del producto debe tener letras")
+            Swal.fire({
+                icon: "error",
+                text: "El nombre del producto debe tener letras"
+            })
+            return;
+        }
+        if (!/^\d+$/.test(precioUnitario)) {
+            Swal.fire({
+                title: "El precio unitario debe contener numeros",
+                icon: "error"
+            });
+            return;
+        }
+        if (!/^\d+$/.test(cantidad)) {
+            Swal.fire({
+                title: "La cantidad del producto debe contener numeros",
+                icon: "error"
+            });
+            return;
+        }
+        if (!/^\d+$/.test(codigo)) {
+            Swal.fire({
+                title: "El codigo del producto debe contener numeros",
+                icon: "error"
+            });
+            return;
+        }
+        if (!/^[a-zA-Z]+$/.test(marca)) {
+            console.log("La Marca del producto debe contener letras")
+            Swal.fire("La Marca del producto debe tener letras");
+            return;
+        }
+        if (!/^[a-zA-Z]+$/.test(UnidadDeMedida)) {
+            console.log("La unidad de medida no debe contener numeros")
+            Swal.fire("La unidad de medida no debe contener numeros");
+            return;
+        }
 
-    if (PrecioUnitario != '' || CantidadProducto != '') {
-        if (PrecioUnitario == /[a-zA-Z]/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        if (CantidadProducto == /[a-zA-Z]/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        else {
-            console.log("ingrese todos los datos")
-        }
-    }                                           
-
-    if (CodigoProducto != '' || MarcaProducto != '') {
-        if (CodigoProducto == /[a-zA-Z]/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        if (MarcaProducto == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        else {
-            console.log("ingrese todos los datos")
-        }
-
-        if (UnidadDeMedida != '') {
-            if (UnidadDeMedida == /[a-zA-Z]/) {
-                console.log("los datos son incorrectos")
-            }
-            else {
-                console.log("ingrese todos los datos ")
-            }
-        }
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Informacion Guardada Correctamente",
+            showConfirmButton: false,
+            timer: 1500
+        });
     }
 }
-
+document.getElementById("btnGuardar").onclick = ValidarInformacion;
--- js/pedidos.js
+++ js/pedidos.js
@@ -1,51 +1,66 @@
-let direccion = document.getElementById("direccion").value; 
-let telefono = document.getElementById("telefono").value; 
-let email = document.getElementById("Email").value; 
-let MetodoDePago = document.getElementById("MetodoDePago").value; 
-let pedido = document.getElementById("pedido").value; 
-let nombre = document.getElementById("nombre").value; 
-let referencias = document.getElementById("referencias").value; 
+function ValidarInformacion() {
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+    let email = document.getElementById("Email").value;
+    let MetodoDePago = document.getElementById("MetodoDePago").value;
+    let pedido = document.getElementById("pedido").value;
+    let nombre = document.getElementById("nombre").value;
+    let referencias = document.getElementById("referencias").value;
 
-function ValidarDatos() {
-    if (direccion != '' || telefono != '') {
-        if (telefono.length >=10) {
-            console.log("los datos ingresados son incorrectos")
-        }
+    if (
+        !direccion || !telefono ||
+        !email || !MetodoDePago ||
+        !pedido || !nombre
+    ) {
+        console.log(
+            `Informacion del Inicio de Sesion: \n
+            ${direccion} \n ${telefono} \n
+            ${email} \n ${MetodoDePago} \n
+            ${pedido} \n ${nombre}`
+        );
+        Swal.fire({
+            title: "campos incompletos",
+            text: "rellena los campos",
+            icon: "question"
+        });
     }
     else {
-        console.log("ingrese todos los datos")
+        console.log(
+            `Informacion del Inicio de Sesion: \n
+            ${direccion} \n ${telefono} \n
+            ${email} \n ${MetodoDePago} \n
+            ${pedido} \n ${nombre}`
+        );
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Informacion Guardada Correctamente",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+    if (!/^\d+$/.test(telefono)) {
+        Swal.fire({
+            title: "El telefono debe contener numeros",
+            icon: "error"
+        });
+        return;
+    }
+    if (!/^[a-zA-Z]+$/.test(Nombre)) {
+        console.log("El nombre debe tener letras")
+        Swal.fire({
+            icon: "error",
+            text: "El nombre debe tener letras"
+        })
+        return;
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        Swal.fire({
+            title: "Ingrese un correo electrónico válido",
+            icon: "error"
+        });
+        return;
     }
 
-    if (Email != '' || MetodoDePago != '') {
-        if (!Email.include('@')) {
-            console.log("los datos ingresados son correctos")
-        }
-        if (MetodoDePago == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        else {
-            console.log("ingrese todos los datos")
-        }
-    }                                           
-
-    if (pedido != '' || nombre != '') {
-        if (pedido == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        if (nombre == /^[^\d]*$/) {
-            console.log("los datos ingresados son incorrectos")
-        }
-        else {
-            console.log("ingrese todos los datos")
-        }
-
-        if (referencias != '') {
-            if (referencias == /^[^\d]*$/) {
-                console.log("los datos son incorrectos")
-            }
-            else {
-                console.log("ingrese todos los datos ")
-            }
-        }
-    }
 }
+document.getElementById("btnGuardar").onclick = ValidarInformacion;
